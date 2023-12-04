df14e62f4096897885182e5ca90f91ea4d04c967
Catalin Murgoci <catalin.catalin729@yahoo.com>
2023-12-04 20:42:56 +0200

    -Added 2 new entities: OnlineOrder, OnlineOrderItem -Added 1 new repository: OnlineOrderRepository

---
+++ src/main/java/ro/ctrln/entities/OnlineOrderItem.java
@@ -0,0 +1,21 @@
+package ro.ctrln.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class OnlineOrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Product product;
+
+    private int quantity;
+}
---
+++ src/main/java/ro/ctrln/repositories/OnlineOrderRepository.java
@@ -0,0 +1,9 @@
+package ro.ctrln.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import ro.ctrln.entities.OnlineOrder;
+
+@Repository
+public interface OnlineOrderRepository extends CrudRepository<OnlineOrder, Long> {
+}
