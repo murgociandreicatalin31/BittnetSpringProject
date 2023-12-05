7d285a5bcf3fc5b0dbb14503f656f9e404b42617
Catalin Murgoci <catalin.catalin729@yahoo.com>
2023-12-05 18:31:34 +0200

    Added OnlineOrderHandler for handling OrderController's Exceptions

---
+++ src/main/java/ro/ctrln/handlers/OnlineOrderHandler.java
@@ -0,0 +1,31 @@
+package ro.ctrln.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import ro.ctrln.exceptions.*;
+
+@ControllerAdvice
+public class OnlineOrderHandler {
+
+    @ExceptionHandler(InvalidProductIdException.class)
+    public ResponseEntity<String> handleInvalidProductIdException() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Product ID is invalid!");
+    }
+
+    @ExceptionHandler(InvalidQuantityException.class)
+    public ResponseEntity<String> handleInvalidQuantityException() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The quantity of a product must be positive!");
+    }
+
+    @ExceptionHandler(NotEnoughStockException.class)
+    public ResponseEntity<String> handleNotEnoughStockException() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("One of the products is out of stock!");
+    }
+
+    @ExceptionHandler(InvalidProductsException.class)
+    public ResponseEntity<String> handleInvalidProductsException() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The order does not contain any products!");
+    }
+}
